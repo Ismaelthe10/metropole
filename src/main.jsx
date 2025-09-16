f16f60c8f4b1ff48d2eb22b1047d94723e30468e
Ismael Monteiro <ismael-dev@outlook.com>
2025-09-16 18:20:18 -0300

    remove helmet in main

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,11 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "../src/styles/globals.css";
 import { BrowserRouter } from "react-router";
-import { HelmetProvider } from "react-helmet-async";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <HelmetProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </HelmetProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>
 );
